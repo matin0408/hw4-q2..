6453d50aea1b1b558ef4650c6ebf3fbbf7c3a96c
matin0408 <150231931+matin0408@users.noreply.github.com>
2023-12-02 00:34:10 +0330

    Add files via upload

---
+++ 123.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+main()  {
+	
+int x,y,z,m;	
+cout <<"please enter number  for x:";
+cin>> x;	
+cout <<"please enter number  for y:";
+cin>> y;	
+cout <<"please enter number  for z:";
+cin>> z;	
+
+m:(x-y)*(x+z) / (x+y+z)*(x+y+z)+1;
+
+cout<<"m:"<< m ;
+}
